09037401aea425c8ec4f339141ee3ed0eab26b91
anuarm <anuarmohaseca@gmail.com>
2017-05-09 12:27:13 +0100

    Added basic Script Movement

---
+++ Assets/MovingController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingController : MonoBehaviour {
+
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+    private float inverseMoveTime;
+    public float moveTime = 0.1f;
+
+    // Use this for initialization
+    protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected bool Move(int x, int y, out RaycastHit2D hit) {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(x, y);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+        if(hit.transform == null) {
+            StartCoroutine(SmoothMovement(end));
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private IEnumerator SmoothMovement(Vector3 end) {
+        float distance = (transform.position - end).sqrMagnitude;
+        while(distance > float.Epsilon) {
+            Vector3 newPosition = Vector3.MoveTowards(rigidBody.position, end, inverseMoveTime * Time.deltaTime);
+
+            rigidBody.MovePosition(newPosition);
+
+            distance = (transform.position - end).sqrMagnitude;
+
+            yield return null;
+        }
+    }
+
+    protected virtual void AttemptMove<T>(int xDir, int yDir) where T : Component {
+        RaycastHit2D hit;
+
+        bool canMove = Move(xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+
+            OnCantMove(hitComponent);
+    }
+
+    protected abstract void OnCantMove<T>(T component)
+        where T : Component;
+
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MovingController {
+
+    private Animator animator;
+
+    protected override void OnCantMove<T>(T component) {
+        throw new NotImplementedException();
+    }
+
+    protected override void Start () {
+        animator = GetComponent<Animator>();
+
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int) Input.GetAxisRaw("Horizontal");
+        vertical = (int)Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0) {
+            AttemptMove<Rock>(horizontal, 0);
+        } else if (vertical != 0) {
+            AttemptMove<Rock>(0, vertical);
+        }
+	}
+}
